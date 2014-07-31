b0ef25a7b164d264e6585e0de4c7176434ec579f
Andy Librian <andylibrian@gmail.com>
2014-07-31 16:42:42 +0700

    initial commit

---
+++ src/IndonesianMinistryTranslation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Laraiba\Translation\Bahasa;
+
+use Laraiba\Resource\Translation\TranslationInterface;
+use Laraiba\Resource\Ayat\Repository\ArrayAyatRepository;
+
+class IndonesianMinistryTranslation extends ArrayAyatRepository implements TranslationInterface
+{
+    public function __construct()
+    {
+    }
+}
